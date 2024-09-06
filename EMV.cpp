beec169b001da333a4269634c7fe331944198042
Saint-Smooth <bdsdcoding@gmail.com>
2024-09-06 14:16:26 +1000

    changes to naming, and logic

--- EMV.cpp
+++ EMV.cpp
@@ -4,9 +4,9 @@
 
 class user_data{
     public:
-    char user_name[40];  /// trivial number
+    char name[40];  /// trivial number
     float balance;
-    char user_address[100];
+    char address[100];
     char location_currency[50];
 
 }; // reminder for semi-colon
@@ -20,29 +20,29 @@ class terminal_PODL{
     char date [8]; /// idk how to create a better time getter. for now only use time stamp. 
     char transaction_type [2];
 
-}
+};
 
 /// maybe another class for the initial payment?
 int main () {
      // Intialised user data
-    user_data user1;
-    strncpy(user1.user_name, "Bob,", sizeof(user1.user_name) - 1);
-    user1.user_name[sizeof(user1.user_name) - 1] = '\0';
+    user_data user1; /// safety measure over char to prevet buffer overflows.
+    strncpy(user1.name, "Bob,", sizeof(user1.name) - 1);
+    user1.name[sizeof(user1.name) - 1] = '\0';
     user1.balance = 120;
-    strncpy(user1.user_address, "11 silly street,", sizeof(user1.user_address) - 1);
-    user1.user_address[sizeof(user1.user_name) - 1] = '\0';
-    strncpy(user1.location_currency, "Switzerland,", sizeof(user1.location_currency) -1);
-    user1.location_currency[sizeof(user1.user_name) - 1] = '\0';
+    strncpy(user1.address, "11 silly street,", sizeof(user1.address) - 1);
+    user1.address[sizeof(user1.address) - 1] = '\0';
+    strncpy(user1.location_currency, "Swiss francx", sizeof(user1.location_currency) -1);
+    user1.location_currency[sizeof(user1.location_currency) - 1] = '\0';
 
     // print statement to make sure all is good.
-    std::cout << user1.user_name << " has " << user1.balance << " Swiz Francs" << " lives in " << user1.user_address << " from " << user1.location_currency << std::endl;
+    std::cout << user1.name << " has " << user1.balance << " Swiz Francs" << " and lives in " << user1.address << " using " << user1.location_currency << std::endl;
     
     
     // intialise payment method request.
     
     int x;
     
-    std::cout << "Hi, customer. What type of method would you like to use? Choose by pressing 1 [contact] or 2 [contactless]:   ";
+    std::cout << "Hi, customer. What type of method would you like to use? Choose by pressing 1 [contact] or 0 [contactless]:   ";
     
     while (true) {
         std::cin >> x;
@@ -52,10 +52,10 @@ int main () {
             std::cin.clear(); // Clear the error flag
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Discard invalid input
             std::cout << "Invalid input. Please enter 1 or 2: ";
-        } else if (x == 1 || x == 2) {
+        } else if (x == 0 || x == 1) {
             break; // Valid input, exit the loop
         } else {
-            std::cout << "Your selection is invalid, please select 1 or 2: ";
+            std::cout << "Your selection is invalid, please select 1 or 0: ";
         }
     }
 
